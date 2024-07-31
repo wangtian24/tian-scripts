57c669d1a582e07ffe84eed65af1636bc365f43f
Gilad Mishne <gilad@yupp.ai>
2024-07-31 10:48:57 -0700

    Add mistral, make nltk local

--- backend/llm/constants.py
+++ backend/llm/constants.py
@@ -1,7 +1,28 @@
-MODELS = [
-    "gpt-4o",
-    "gpt-4o-mini",
-    "mistral-large-latest",
-    "gemini-1.5-pro",
-    "claude-3-5-sonnet-20240620",
-]
+from enum import Enum
+
+
+class ChatProvider(Enum):
+    OPENAI = 1
+    ANTHROPIC = 2
+    GOOGLE = 3
+    MISTRAL = 4
+
+    @classmethod
+    def from_string(cls, provider: str) -> "ChatProvider":
+        try:
+            return cls[provider.upper()]
+        except KeyError as e:
+            raise ValueError(f"Unsupported provider string: {provider}") from e
+
+
+MODELS_BY_PROVIDER = {
+    ChatProvider.OPENAI: [
+        "gpt-4o",
+        "gpt-4o-mini",
+    ],
+    ChatProvider.MISTRAL: ["mistral-large-latest"],
+    ChatProvider.GOOGLE: ["gemini-1.5-pro"],
+    ChatProvider.ANTHROPIC: ["claude-3-5-sonnet-20240620"],
+}
+
+MODELS = [model for models in MODELS_BY_PROVIDER.values() for model in models]
