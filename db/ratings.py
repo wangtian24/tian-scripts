6296fc935d4891b948f14bfb3f58172e8c4245d2
Wenny Yustalim <18345286+wennyyustalim@users.noreply.github.com>
2024-08-21 17:19:42 -0700

    Add a Score table. (#50)
    ref YUP-265

--- db/ratings.py
+++ db/ratings.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import ForeignKey, String, Uuid
+from sqlalchemy import Float, ForeignKey, String, UniqueConstraint, Uuid
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from db.base import BaseModel
@@ -29,3 +29,23 @@ class Category(BaseModel):
             name_parts.append(parent.name)
             parent = parent.parent_category
         return " > ".join(reversed(name_parts))
+
+
+# A rating is a score for a model in a category.
+class Rating(BaseModel):
+    __tablename__ = "ratings"
+
+    rating_id = mapped_column(Uuid(as_uuid=True), primary_key=True, nullable=False, default=uuid.uuid4)
+    model_id = mapped_column(Uuid(as_uuid=True), ForeignKey("language_models.model_id"), nullable=False)
+    category_id = mapped_column(Uuid(as_uuid=True), ForeignKey("categories.category_id"), nullable=False)
+    score = mapped_column(Float, nullable=False, default=0)
+
+    # The lower and upper bounds of the 95% confidence interval for the score.
+    lower_bound_95 = mapped_column(Float, nullable=False, default=0)
+    upper_bound_95 = mapped_column(Float, nullable=False, default=0)
+
+    model = relationship("LanguageModel", back_populates="ratings")
+    category = relationship("Category", back_populates="ratings")
+
+    # A model can have different ratings for different categories.
+    __table_args__ = (UniqueConstraint("model_id", "category_id", name="uq_model_category"),)
