37e0b2b594ec1a2d95a42a791be05393c977e9b5
retired-Hurt <ansuman@yupp.ai>
2024-09-15 21:21:40 +0530

    moved the parameters to body of the request as opposed to query params (#144)

--- ypl/backend/routes/v1/rank.py
+++ ypl/backend/routes/v1/rank.py
@@ -1,6 +1,7 @@
 import logging
 
-from fastapi import APIRouter, HTTPException, Query
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 
 from ypl.backend.llm.ranking import RatedModel, can_use_global_rankers, get_default_ranker, get_ranker
 
@@ -9,18 +10,20 @@ logger.setLevel(logging.INFO)
 router = APIRouter()
 
 
+class LeaderboardRequest(BaseModel):
+    category_names: list[str] | None
+    exclude_ties: bool = False
+    language: str | None = None
+    model_names: list[str] | None = None
+
+
 @router.post("/leaderboard")
-def leaderboard(
-    category_names: list[str] | None = Query(default=None, description="The categories to filter by"),  # noqa: B008
-    exclude_ties: bool = Query(default=False, description="Whether to exclude ties"),
-    language: str | None = Query(default=None, description="The language to filter by"),
-    model_names: list[str] | None = Query(default=None, description="The models to filter by"),  # noqa: B008
-) -> dict[str, list[RatedModel]] | list[RatedModel]:
+async def leaderboard(request: LeaderboardRequest) -> dict[str, list[RatedModel]] | list[RatedModel]:
     try:
-        if can_use_global_rankers(category_names, exclude_ties, language, model_names):
+        if can_use_global_rankers(request.category_names, request.exclude_ties, request.language, request.model_names):
             return get_ranker().leaderboard_all_categories()
         ranker = get_default_ranker()
-        ranker.add_evals_from_db(category_names, exclude_ties, language, model_names)
+        ranker.add_evals_from_db(request.category_names, request.exclude_ties, request.language, request.model_names)
         return ranker.leaderboard()
     except Exception as e:
         logger.error(f"Error updating rankings: {e}")
