1c68448c8adf9008f71cc1236de8e6fa1972da63
retired-Hurt <ansuman@yupp.ai>
2024-09-21 12:18:14 +0530

    added origin check (#167)

--- ypl/backend/server.py
+++ ypl/backend/server.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from collections.abc import Callable
+
+from fastapi import FastAPI, HTTPException, Request, Response
 from fastapi.responses import ORJSONResponse
 from fastapi.routing import APIRoute
 from starlette.middleware.cors import CORSMiddleware
@@ -18,6 +20,16 @@ app = FastAPI(
     generate_unique_id_function=custom_generate_unique_id,
     default_response_class=ORJSONResponse,
 )
+
+
+@app.middleware("http")
+async def origin_header_check(request: Request, call_next: Callable) -> Response:
+    origin = request.headers.get("origin")
+    if origin is None:
+        raise HTTPException(status_code=400, detail="Origin header is required")
+    response: Response = await call_next(request)
+    return response
+
 
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
