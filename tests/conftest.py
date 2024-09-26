f00ca16f3cb00a969d9c229bec6fcffdc9f6d593
Aravind Pedapudi <aravindp1510@gmail.com>
2024-09-26 13:06:45 +0530

    Enable alembic tests after fixing the tests to support new extensions (#182)
    Also modify the test config to run whenever there's a related test change too

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,18 @@
 import pytest
 from pytest_alembic.config import Config
-from pytest_mock_resources import create_postgres_fixture
+from pytest_mock_resources import PostgresConfig, create_postgres_fixture
 
 
 @pytest.fixture
 def alembic_config() -> Config:
     """Override this fixture to configure the exact alembic context setup required."""
     return Config()
+
+
+@pytest.fixture(scope="session")
+def pmr_postgres_config() -> PostgresConfig:
+    # As we add more extensions, we will need to have a custom image that has all the extensions installed
+    return PostgresConfig(image="pgvector/pgvector:pg15")  # type: ignore
 
 
 alembic_engine = create_postgres_fixture()  # type: ignore
--- ypl/logger.py
+++ ypl/logger.py
@@ -3,5 +3,5 @@ from ypl.backend.config import settings
 if settings.USE_GOOGLE_CLOUD_LOGGING:
     import google.cloud.logging
 
-    client = google.cloud.logging.Client()  # type: ignore
-    client.setup_logging()  # type: ignore
+    client = google.cloud.logging.Client()
+    client.setup_logging()
