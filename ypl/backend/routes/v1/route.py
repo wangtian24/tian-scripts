4613351c5fa4d6e71377458660c5462915d8fff5
Minqi Chen <1323561+BenMQ@users.noreply.github.com>
2024-10-16 19:51:29 -0700

    Proposed new API to include user preference (which model is the winner) in routing. (#278)
    **Looking for feedback**: should we allow only 1 model to be the "preferred" model, or communicate a score per model instead? i.e. alternative proposal is send {modelName: score} where selected model gets a 1, unselected gets a -1 (or multiply by a factor of 100)? It's more complex, and we don't need it right now. but may need it in the future.
    
    The general idea is that when user says model A is better than model B and sends a follow up, we should intelligently route the response to
    model A and a new model C.
    
    This is preferred over sending a chat ID for the router to look up user selection, because that would be too slow.
    
    ![Screenshot 2024-10-16 at 11.58.06 AM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/tYruKgqG9owpRl5fwTto/4897a428-285d-42ff-a66f-e46d6cb911d8.png)

--- ypl/backend/routes/v1/route.py
+++ ypl/backend/routes/v1/route.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from fastapi import APIRouter, Body, Query
+from pydantic import BaseModel, Field
 from tqdm import tqdm
 
 from ypl.backend.config import settings
@@ -10,11 +11,27 @@ from ypl.backend.llm.routing.router import RouterState, get_prompt_conditional_r
 router = APIRouter()
 
 
+class PreferredModel(BaseModel):
+    models: list[str] = Field(description="List of models presented to the user for a given turn.")
+    preferred: str | None = Field(description="Which model was preferred by the user, or None if all are bad")
+
+
+class RoutingPreference(BaseModel):
+    turns: list[PreferredModel] | None = Field(
+        description=(
+            "The preference for each of the past turns in the chat context "
+            "in chronological order (first turn is the oldest). "
+            "An empty list indicates that there were no prior turns."
+        )
+    )
+
+
 @router.post("/select_models")
 async def select_models(
     prompt: str = Query(..., description="Prompt"),
     num_models: int = Query(default=2, description="Number of different models to route to"),
     budget: float = Query(default=float("inf"), description="Budget"),
+    preference: None | RoutingPreference = Body(default=None, description="List of past outcomes"),  # noqa: B008
 ) -> list[str]:
     if settings.ROUTING_USE_PROMPT_CONDITIONAL:
         router = get_prompt_conditional_router(prompt, num_models)
