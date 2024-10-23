600506e9becdd7fa8f8895cb8c529f0e9be1a8f9
Wenny Yustalim <18345286+wennyyustalim@users.noreply.github.com>
2024-10-23 16:25:43 -0700

    Add EvalType enum: SELECTION, ALL_BAD, and QUICK_TAKE. These will replace all the existing eval types aft er the migration. The old eval types are kept for backwards compatibility for now. (#319)

--- ypl/db/chats.py
+++ ypl/db/chats.py
@@ -173,16 +173,25 @@ class ChatMessage(BaseModel, table=True):
 
 
 class EvalType(enum.Enum):
+    # TODO: Will be deprecated after migration --> replaced by SELECTION.
     # Primitive evaluation of two responses where the user distributes 100 points between two responses.
     # The eval result is a dictionary where the key is the model name and the value is the number of points.
     SLIDER_V0 = "slider_v0"
+    # TODO: Will be deprecated after migration --> replaced by QUICK_TAKE.
     # Thumbs up / thumbs down evaluation applied to a
     # single message. A positive value is thumbs up, and
     # negative value is thumbs down.
     THUMBS_UP_DOWN_V0 = "thumbs_up_down_v0"
+    # TODO: Will be deprecated after migration --> replaced by QUICK_TAKE.
     # User-generated alternative to a Quick Take produced
     # by a model.
     QUICK_TAKE_SUGGESTION_V0 = "quick_take_suggestion_v0"
+    # Eval where user selects one winner from multiple responses.
+    SELECTION = "selection"
+    # Eval where user indicates all responses are bad.
+    ALL_BAD = "all_bad"
+    # QuickTake eval can contain thumbs up/down and/or comment.
+    QUICK_TAKE = "quick_take"
 
 
 class MessageEval(BaseModel, table=True):
