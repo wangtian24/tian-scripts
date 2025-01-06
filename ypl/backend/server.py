a5c5f404c4ce55e8eb3fc22862389a4d80c97d92
BhanuTejaCH <bhanu@yupp.ai>
2025-01-06 10:18:37 +0530

    add 422 error details to server logs (#736)
    * add 422 error details to server logs
    
    * formatting imports

--- ypl/backend/server.py
+++ ypl/backend/server.py
@@ -1,7 +1,9 @@
+import logging
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
-from fastapi.responses import ORJSONResponse
+from fastapi import FastAPI, Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse, ORJSONResponse
 from fastapi.routing import APIRoute
 from starlette.middleware.cors import CORSMiddleware
 
@@ -39,6 +41,16 @@ app = FastAPI(
     default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError) -> JSONResponse:
+    # Log the error details
+    error_details = str(exc.errors())
+    logging.warn(f"422 Validation error: {error_details}")
+
+    # Return error response
+    return JSONResponse(status_code=422, content={"detail": exc.errors(), "body": exc.body})
 
 
 # Set all CORS enabled origins
