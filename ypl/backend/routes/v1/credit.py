ed4ac8b0489084325234d8c856fa79371896b213
Aravind Pedapudi <aravindp1510@gmail.com>
2025-01-10 21:25:30 +0530

    Update the YUPP-INR conversion value. (#819)
    ref ENG-1532

--- ypl/backend/routes/v1/credit.py
+++ ypl/backend/routes/v1/credit.py
@@ -62,7 +62,7 @@ class CashoutCreditsRequest:
 
 async def convert_credits_to_currency(credits: int, currency: CurrencyEnum) -> Decimal:
     # TODO: Put them in a config somewhere.
-    CREDITS_TO_INR_RATE = Decimal("0.1")
+    CREDITS_TO_INR_RATE = Decimal("0.05")
     CREDITS_TO_USD_RATE = Decimal("0.0012")
 
     credits_decimal: Decimal = Decimal(credits)
