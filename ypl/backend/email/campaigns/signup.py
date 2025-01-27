a86eb502ec8945de2429348f7f1c1c2aa41d9871
Wenny Yustalim <18345286+wennyyustalim@users.noreply.github.com>
2025-01-26 19:00:31 -0800

    Update existing email templates to follow Yupp Automated Emails (#1035)
    https://docs.google.com/document/d/19T2H0cuE4V1puV6sVxJeHkMJ8i4qH3OmVFX-PWrxv2E/edit?tab=t.0

--- ypl/backend/email/campaigns/signup.py
+++ ypl/backend/email/campaigns/signup.py
@@ -1,100 +1,101 @@
-SIC_AVAILABILITY_EMAIL_TITLE = "Thanks for using Yupp (Beta). New from us just for you – share Yupp with your friends!"
+################################################################################
+
+
+SIC_AVAILABILITY_EMAIL_TITLE = "Share Yupp with your friends!"
 SIC_AVAILABILITY_EMAIL_TEMPLATE = """
 Hey {name},
 
-Thanks for being a valuable Trusted Tester of Yupp. Your feedback so far has given us invaluable insights and we continue to iterate on the product! We hope you are enjoying your experience and finding Yupp useful, and ask for your strict confidentiality in this phase 🙏.
+We just gifted you with your own referral code!
 
-We are thrilled to release our ‘referral’ feature. With this functionality, you would be able to invite your friends, using a special invite code created just for you. This feature would start rolling out to other users in the upcoming weeks, but we thought it would be great to let you in early.
+Now you can invite your friends to try Yupp (Alpha):
+From your profile picture on the top right corner, click “Refer your friends” to see your special invite code.
+Ask your friends to visit https://gg.yupp.ai, login using their Google account, and enter your invite code when prompted. Note: You can currently invite up to 3 friends per week.
 
-Click on your profile picture, and you will see “Refer your friends”. When you click on it, you will see your special invite code.
 
-Ask your friends to visit https://gg.yupp.ai, login using their gmail id, and enter your invite code when prompted. And that’s it, they are in. Note: There’s a limit on the number of friends you can invite per week.
+For each friend you refer, you’ll get 10,000 YUPP credits and your friend will get 1,000. Please don’t refer to journalists, investors, competitors, etc and continue to keep the product confidential. We want to remain in stealth for now.
 
-And well, that’s not all - the referral program gets you 10000 extra YUPP credits and your friend 1000! Please refer your friends now.  One extra request - please don’t refer to journalists, investors, competitors etc. and continue to keep the product confidential. We want to remain in stealth for now.
+Thanks for actively using Yupp, we hope you’re enjoying your experience and finding Yupp useful.
 
-Thank you for being a part of this journey with us 🫶🏼.
-
-Cheers,
-
-Mouli
-[from the Product team]
+- Mouli, product team
 """  # noqa: E501
 
 SIC_AVAILABILITY_EMAIL_TEMPLATE_HTML = """
 <p>Hey {name},</p>
 
-<p>Thanks for being a valuable Trusted Tester of Yupp. Your feedback so far has given us invaluable insights and we continue to iterate on the product! We hope you are enjoying your experience and finding Yupp useful, and ask for your strict confidentiality in this phase 🙏.</p>
+<p>We just gifted you with your own referral code!</p>
 
-<p>We are thrilled to release our ‘referral’ feature. With this functionality, you would be able to invite your friends, using a special invite code created <strong>just for you</strong>. This feature would start rolling out to other users in the upcoming weeks, <strong>but we thought it would be great to let you in early</strong>.</p>
+<p>Now you can invite your friends to try Yupp (Alpha):</p>
 
 <ol>
-<li>Click on your profile picture, and you will see “Refer your friends”. When you click on it, you will see your special invite code.</li>
-<li>Ask your friends to visit <a href="https://gg.yupp.ai">https://gg.yupp.ai</a>, login using their gmail id, and enter your invite code when prompted. And that’s it, they are in. <em>Note: There’s a limit on the number of friends you can invite per week.</em></li>
+<li>From your profile picture on the top right corner, click “Refer your friends” to see your special invite code.</li>
+<li>Ask your friends to visit <a href="https://gg.yupp.ai">https://gg.yupp.ai</a>, login using their Google account, and enter your invite code when prompted. <em>Note: You can currently invite up to 3 friends per week.</em></li>
 </ol>
 
-<p>And well, that’s not all - the referral program gets you 10000 extra YUPP credits and your friend 1000! Please refer your friends now.  One extra request - please don’t refer to journalists, investors, competitors etc. and continue to keep the product confidential. We want to remain in stealth for now.</p>
+<p>For each friend you refer, you’ll get 10,000 YUPP credits and your friend will get 1,000. Please don’t refer to journalists, investors, competitors, etc and continue to keep the product confidential. We want to remain in stealth for now.</p>
 
-<p>Thank you for being a part of this journey with us 🫶🏼.</p>
+<p>Thanks for actively using Yupp, we hope you’re enjoying your experience and finding Yupp useful.</p>
 
-<p>Cheers,</p>
-
-<p>Mouli<br/>[from the Product team]</p>
+- Mouli, product team
 """  # noqa: E501
 
 
-SIGN_UP_EMAIL_TITLE = "Thanks for joining Yupp (Beta) as an early Trusted Tester."
+################################################################################
+
+
+SIGN_UP_EMAIL_TITLE = "Thanks for joining Yupp (Alpha) as an early Trusted Tester"
 SIGN_UP_EMAIL_TEMPLATE = """
 Hey {name},
 
-We’re thrilled to have you as an early Trusted Tester user! Your feedback will play a crucial role in refining the product, and we can’t wait to hear your thoughts.
+We’re thrilled to have you here!
 
-Thank you for being a part of this small, exclusive group with access to the product in its private, closed beta. We really appreciate your trust and feedback, and ask for your strict confidentiality in this phase 🙏.
+A few important points before you dive in:
+• Confidential Access: This early access is exclusive to you, so please refrain from sharing it with others. Your discretion is greatly appreciated.
+• Logging: To help us improve, we’ll be logging extensively. Please avoid writing any prompts that you consider confidential.
+• We are still in alpha: That means you may encounter some rough edges or unexpected issues. We appreciate your patience as we work through it!
 
-Before you dive in, here are a few important points to note:
-• Confidential Access: This early access is exclusive to you, so please refrain from sharing it with others. Your discretion is greatly appreciated. 🔐
-• Logging: To help us improve, we’ll be logging extensively. Please avoid writing any prompts that you consider confidential. 🔍
-• It’s early: The product is in its early stages, so you may encounter some rough edges or unexpected issues. We appreciate your patience as we work to improve!
-• Try it out now! Send a prompt - whatever's on your mind - to the AIs, and share a quick feedback on which one you prefer. We will give you a one-time 1000 YUPP credits as a bonus.
+Try it out now – send a prompt to the AIs and pick the one you prefer. We will give you a one-time 1,000 YUPP credits as a treat.
 
-For updates, AI discussions with our devs (and other Trusted Testers like yourself), and funny memes, join our nascent Discord community here: https://discord.gg/AGHSbyqgXP
+For updates, AI discussions with our devs (and other Trusted Testers), and funny memes, join our Discord community here: https://discord.gg/AGHSbyqgXP
 
-Your inputs are invaluable, and we’re excited to hear your thoughts!
+Your feedback plays a crucial role in refining the product, we can’t wait to hear your thoughts.
 
-Thank you for being a part of this journey with us 🫶🏼.
+- Mouli, product team
 
-Cheers,
 
-Mouli
-[from the Product team]
+----
+Thanks for being a part of our small, invite-only alpha. We really appreciate your trust and ask for your strict confidentiality.
 """  # noqa: E501
 
+
+################################################################################
+# TODO: Add "Please continue to refer more of your friends by sharing <this URL with promo code> with them!"
 
 REFFERAL_BONUS_EMAIL_TITLE = "You just scored a referral bonus of {credits} YUPP credits!"
 REFFERAL_BONUS_EMAIL_TEMPLATE = """
 Hey {referrer_name},
 
-We’re happy to let you know that {referee_name}, whom you referred, has signed up successfully and has started using Yupp. This granted you a referral bonus of {credits} YUPP credits!
+{referee_name} has signed up using your invite code and has started using Yupp–granting you a referral bonus of {credits} YUPP credits.
 
-Cheers,
-Mouli
-[from the Product team]
+- Mouli, product team
+
 
 ----
-
-Thank you for being a part of this small, exclusive group with access to the product in its private, closed beta. We really appreciate your trust and feedback, and ask for your strict confidentiality in this phase 🙏.
+Thanks for being a part of our small, invite-only alpha. We really appreciate your trust and ask for your strict confidentiality.
 """  # noqa: E501
+
+
+################################################################################
 
 
 REFFERED_USER_EMAIL_TITLE = "You just scored {credits} YUPP credits!"
 REFFERED_USER_EMAIL_TEMPLATE = """
 Hey {name},
 
-Thank you for providing your first "I prefer this" feedback! We’re happy to let you know that since you joined Yupp with a special invite code, that granted yourself a bonus of {credits} YUPP credits!
+Thank you for giving your first feedback to the AI models. Since you joined Yupp with your friend’s invite code, you’ve been rewarded {credits} YUPP credits!
 
-Cheers,
-Mouli
-[from the Product team]
+- Mouli, product team
+
+
 ----
-
-Thank you for being a part of this small, exclusive group with access to the product in its private, closed beta. We really appreciate your trust and feedback, and ask for your strict confidentiality in this phase 🙏.
+Thanks for being a part of our small, invite-only alpha. We really appreciate your trust and ask for your strict confidentiality.
 """  # noqa: E501
