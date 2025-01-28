fb65773a717beceace7148619e7af14b0c8f8c2b
Wenny Yustalim <18345286+wennyyustalim@users.noreply.github.com>
2025-01-27 17:32:30 -0800

    Add nudge & summary email templates/campaigns (#1073)
    Fixes ENG-1897
    
    https://docs.google.com/document/d/19T2H0cuE4V1puV6sVxJeHkMJ8i4qH3OmVFX-PWrxv2E/edit?tab=t.0

---
+++ ypl/backend/email/campaigns/nudge.py
@@ -0,0 +1,142 @@
+################################################################################
+
+
+WEEK_1_CHECKIN_EMAIL_TITLE = "A quick check-in and important reminder"
+WEEK_1_CHECKIN_EMAIL_PREVIEW = "Tell us your experience so far and please keep things under wraps"
+WEEK_1_CHECKIN_EMAIL_TEMPLATE = """
+Hey {name},
+
+Thanks for joining {BRAND_NAME}—we're stoked to have you on board.
+
+How's your experience been so far? Any feedback or questions for us?
+
+Also just a friendly reminder, {BRAND_NAME} is still in stealth mode. This means we're still keeping {BRAND_NAME} under wraps for now. Please keep your experience confidential and don't share details on social media (or elsewhere) just yet.
+
+Thanks for your cooperation!
+
+- Mouli, product team
+
+"""  # noqa: E501
+
+WEEK_1_CHECKIN_EMAIL_TEMPLATE_HTML = """
+<p>Hey {name},</p>
+
+<p>Thanks for joining {BRAND_NAME}—we're stoked to have you on board.</p>
+
+<p>How's your experience been so far? Any feedback or questions for us?</p>
+
+<p>Also just a friendly reminder, {BRAND_NAME} is still in stealth mode. This means we're still keeping {BRAND_NAME} under wraps for now. Please keep your experience confidential and don't share details on social media (or elsewhere) just yet.</p>
+
+<p>Thanks for your cooperation!</p>
+
+- Mouli, product team
+"""  # noqa: E501
+
+
+################################################################################
+
+
+WEEK_1_INACTIVE_EMAIL_TITLE = "It's been a minute"
+WEEK_1_INACTIVE_EMAIL_PREVIEW = "We noticed you haven't tried {BRAND_NAME} yet. Let's get you started!"
+WEEK_1_INACTIVE_EMAIL_TEMPLATE = """
+Hey {name}
+
+It's been a week since you created your {BRAND_NAME} account, but you haven't actually used the app yet.
+
+We'd love to learn what your first impression of {BRAND_NAME} is, what do you think of our onboarding flow?
+
+Anyways, using {BRAND_NAME} is easy! If you want a walkthrough, we're more than happy to show you.
+
+- Mouli, product team
+
+{CONFIDENTIALITY_FOOTER}
+"""  # noqa: E501
+
+WEEK_1_INACTIVE_EMAIL_TEMPLATE_HTML = """
+<p>Hey {name}</p>
+
+<p>It's been a week since you created your {BRAND_NAME} account, but you haven't actually used the app yet.</p>
+
+<p>We'd love to learn what your first impression of {BRAND_NAME} is, what do you think of our onboarding flow?</p>
+
+<p>Anyways, using {BRAND_NAME} is easy! If you want a walkthrough, we're more than happy to show you.</p>
+
+<p>- Mouli, product team</p>
+
+<hr>
+
+<p>Thanks for being a part of our small, invite-only alpha.<br>
+We really appreciate your trust and ask for your strict confidentiality.</p>
+"""  # noqa: E501
+
+
+################################################################################
+
+
+WEEK_5_INACTIVE_EMAIL_TITLE = "Are you still there?"
+WEEK_5_INACTIVE_EMAIL_PREVIEW = "Stop your account from being deactivated"
+WEEK_5_INACTIVE_EMAIL_TEMPLATE = """
+Hey {name},
+
+It's been 5 weeks since we last saw you on {BRAND_NAME}. Just checking in to see if you're still around—your AI crew misses you!
+
+If you're still interested in using {BRAND_NAME}, we'd love to have you back! However, if you're not planning on using the app, we'll be deactivating your account in about a week's time. No big deal, just a heads up.
+We do this to minimize the surface area while we are in the early alpha phase of the product.
+
+Let us know if there's anything we can do to make {BRAND_NAME} work better for you.
+
+- Mouli, product team
+
+{CONFIDENTIALITY_FOOTER}
+"""  # noqa: E501
+
+WEEK_5_INACTIVE_EMAIL_TEMPLATE_HTML = """
+<p>Hey {name},</p>
+
+<p>It's been 5 weeks since we last saw you on {BRAND_NAME}. Just checking in to see if you're still around—your AI crew misses you!</p>
+
+<p>If you're still interested in using {BRAND_NAME}, we'd love to have you back! However, if you're not planning on using the app, we'll be deactivating your account in about a week's time. No big deal, just a heads up.</p>
+<p>We do this to minimize the surface area while we are in the early alpha phase of the product.</p>
+
+<p>Let us know if there's anything we can do to make {BRAND_NAME} work better for you.</p>
+
+<p>- Mouli, product team</p>
+
+<hr>
+
+<p>Thanks for being a part of our small, invite-only alpha.<br>
+We really appreciate your trust and ask for your strict confidentiality.</p>
+"""  # noqa: E501
+
+
+################################################################################
+
+
+WEEK_6_DEACTIVATION_EMAIL_TITLE = "Your {BRAND_NAME} account has been deactivated"
+WEEK_6_DEACTIVATION_EMAIL_PREVIEW = "Due to inactivity, we've deactivated your account—but you can always come back!"
+WEEK_6_DEACTIVATION_EMAIL_TEMPLATE = """
+Hey {name},
+
+Just a heads-up—your {BRAND_NAME} account has been deactivated after 6 weeks of inactivity. We had to free up space for others, but no worries, it's all good.
+
+If you ever want to jump back into chatting with AIs, getting rewards, and shaping the future of {BRAND_NAME}, you can sign up again anytime. We'd love to have you back!
+
+Until then, take care and keep doing your thing.
+
+- Mouli, product team
+"""  # noqa: E501
+
+WEEK_6_DEACTIVATION_EMAIL_TEMPLATE_HTML = """
+<p>Hey {name},</p>
+
+<p>Just a heads-up—your {BRAND_NAME} account has been deactivated after 6 weeks of inactivity. We had to free up space for others, but no worries, it's all good.</p>
+
+<p>If you ever want to jump back into chatting with AIs, getting rewards, and shaping the future of {BRAND_NAME}, you can sign up again anytime. We'd love to have you back!</p>
+
+<p>Until then, take care and keep doing your thing.</p>
+
+<p>- Mouli, product team</p>
+"""  # noqa: E501
+
+
+################################################################################
---
+++ ypl/backend/email/campaigns/summary.py
@@ -0,0 +1,66 @@
+MONTHLY_RECAP_EMAIL_TITLE = "Your {BRAND_NAME} Monthly Recap"
+MONTHLY_RECAP_EMAIL_PREVIEW = "Check out your {BRAND_NAME} activity from last month"
+MONTHLY_RECAP_EMAIL_TEMPLATE = """
+Hey {name},
+
+Recap time—a quick snapshot of how you've been using {BRAND_NAME}. Let's dive in:
+
+✨ Your Top 3 Prompts
+
+{prompt_1}
+{prompt_2}
+{prompt_3}
+
+✨ Feedback Frenzy
+
+You gave {pref_count} feedback on AI models—seems like you've got a good eye for quality responses.
+
+✨ Favorite Model
+
+Your go-to AI this month was {model_name}. Looks like you two really vibe!
+
+✨ Credits Update
+
+Received: {credits_received} credits
+Cashed out: {credits_spent} credits
+
+Keep up the great work, and here's to another month of smarter chats and better AIs. Let us know if you have any questions or feedback!
+
+- Mouli, product team
+
+{CONFIDENTIALITY_FOOTER}
+"""  # noqa: E501
+
+MONTHLY_RECAP_EMAIL_TEMPLATE_HTML = """
+<p>Hey {name},</p>
+
+<p>Recap time—a quick snapshot of how you've been using {BRAND_NAME}. Let's dive in:</p>
+
+<p>✨ Your Top 3 Prompts</p>
+
+<p>{prompt_1}<br>
+{prompt_2}<br>
+{prompt_3}</p>
+
+<p>✨ Feedback Frenzy</p>
+
+<p>You gave {pref_count} feedback on AI models—seems like you've got a good eye for quality responses.</p>
+
+<p>✨ Favorite Model</p>
+
+<p>Your go-to AI this month was {model_name}. Looks like you two really vibe!</p>
+
+<p>✨ Credits Update</p>
+
+<p>Received: {credits_received} credits<br>
+Cashed out: {credits_spent} credits</p>
+
+<p>Keep up the great work, and here's to another month of smarter chats and better AIs. Let us know if you have any questions or feedback!</p>
+
+<p>- Mouli, product team</p>
+
+{CONFIDENTIALITY_FOOTER}
+"""  # noqa: E501
+
+
+################################################################################
