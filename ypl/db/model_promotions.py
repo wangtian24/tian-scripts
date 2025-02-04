3f4d9c9f6936ecfee91896aa902eca64d7b2023c
Tian Wang <tian@yupp.ai>
2025-02-03 23:21:38 -0800

    [promo] add promotion status (#1197)
    add promo status

--- ypl/db/model_promotions.py
+++ ypl/db/model_promotions.py
@@ -1,12 +1,18 @@
 import uuid
 from datetime import datetime
+from enum import Enum
 
 import sqlalchemy as sa
-from sqlalchemy import Boolean, Column
+from sqlalchemy import Column
 from sqlmodel import Field, Relationship
 
 from ypl.db.base import BaseModel
 from ypl.db.language_models import LanguageModel
+
+
+class ModelPromotionStatus(str, Enum):
+    ACTIVE = "ACTIVE"
+    INACTIVE = "INACTIVE"
 
 
 class ModelPromotion(BaseModel, table=True):
@@ -22,8 +28,8 @@ class ModelPromotion(BaseModel, table=True):
     # The language model used to generate the message. Set when the message_type is assistant_message.
     language_model_id: uuid.UUID | None = Field(foreign_key="language_models.language_model_id", nullable=False)
 
-    # Whether this model is being promoted
-    in_promo: bool | None = Field(sa_column=Column(Boolean(), nullable=False, default=False))
+    promo_status: ModelPromotionStatus = Field(default=ModelPromotionStatus.INACTIVE, nullable=False)
+
     # When the promotion starts
     promo_start_date: datetime | None = Field(sa_column=Column(sa.DateTime(timezone=True), nullable=True, default=None))
     # When the promotion ends
