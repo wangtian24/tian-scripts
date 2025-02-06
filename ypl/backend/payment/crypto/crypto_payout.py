1a08ccf4738dc24656ba93edc664094fcec919c0
retired-Hurt <ansuman@yupp.ai>
2025-02-06 13:11:09 +0530

    comment reputation score change (#1244)
    comment reputation score change

--- ypl/backend/payment/crypto/crypto_payout.py
+++ ypl/backend/payment/crypto/crypto_payout.py
@@ -8,7 +8,6 @@ from typing import Any, Final
 
 import httpx
 from cdp import Cdp, Transfer, Wallet
-from cdp.address import Address
 from ypl.backend.config import settings
 from ypl.backend.llm.utils import post_to_slack
 from ypl.backend.payment.crypto.crypto_wallet import load_wallet_data
@@ -203,18 +202,19 @@ class CryptoRewardProcessor:
             start_time = time.time()
 
             # check for risky address
-            external_address = Address("base-mainnet", reward.wallet_address)
-            address_reputation = external_address.reputation()
-            if address_reputation.risky:
-                log_dict = {
-                    "message": "Coinbase Crypto Payout: Risky address detected",
-                    "address": reward.wallet_address,
-                    "risky": address_reputation.risky,
-                    "risk_score": address_reputation.score,
-                    "risk_type": address_reputation.metadata,
-                }
-                logging.warning(json_dumps(log_dict))
-                raise CryptoPayoutError(GENERIC_ERROR_MESSAGE, {"error": "Risky address"})
+            # comment out for now as we are not using it for now. however we will use it in the future.
+            # external_address = Address("base-mainnet", reward.wallet_address)
+            # address_reputation = external_address.reputation()
+            # if address_reputation.risky:
+            #     log_dict = {
+            #         "message": "Coinbase Crypto Payout: Risky address detected",
+            #         "address": reward.wallet_address,
+            #         "risky": address_reputation.risky,
+            #         "risk_score": address_reputation.score,
+            #         "risk_type": address_reputation.metadata,
+            #     }
+            #     logging.warning(json_dumps(log_dict))
+            #     raise CryptoPayoutError(GENERIC_ERROR_MESSAGE, {"error": "Risky address"})
 
             if not self.wallet:
                 new_processor = await get_processor()
