301886b28d5fb2fc6d2ee7a8dda09706d1667b81
retired-Hurt <ansuman@yupp.ai>
2025-03-11 10:30:13 +0530

    include the stripe router for webhook (#1772)
    include the stripe router for webhook

--- ypl/webhooks/routes/main.py
+++ ypl/webhooks/routes/main.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from ypl.webhooks.routes.v1 import coinbase, paypal
+from ypl.webhooks.routes.v1 import coinbase, paypal, stripe
 
 api_router = APIRouter()
 
 # Include all webhook routes
 api_router.include_router(coinbase.router, prefix="/coinbase", tags=["coinbase"])
 api_router.include_router(paypal.router, prefix="/paypal", tags=["paypal"])
+api_router.include_router(stripe.router, prefix="/stripe", tags=["stripe"])
